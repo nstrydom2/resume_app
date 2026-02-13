9549ae390822c085a06f86de97c6e7d8525ad855
Nicholas Strydom <nstrydom@gmail.com>
2026-02-13 06:20:14 +0000

    [Dev] redundancy commit

--- main/views.py
+++ main/views.py
@@ -39,7 +39,7 @@ class ContactView(generic.FormView):
 	
 	def form_valid(self, form):
 		form.save()
-		messages.success(self.request, 'Thank you. We will be in touch soon.')
+		messages.success(self.request, 'Thank you. I will be in touch soon.')
 		return super().form_valid(form)
 
 
---
+++ middleware.py
@@ -0,0 +1,16 @@
+import logging
+
+logger = logging.getLogger("connections")
+
+class LogIPMiddleware:
+	def __init__(self, get_response):
+		self.get_response = get_response
+
+	def __call__(self, request):
+		ip = request.META.get("HTTP_X_FORWARDED_FOR:)
+		if ip:
+			ip = ip.split(",")[0].strip()
+		else:
+			ip = request.META.get("REMOTE_ADDR")
+		logger.info("IP=%s PATH=%s METHOD=%s", ip, request.path, request.method)
+		return self.get_response(request)
